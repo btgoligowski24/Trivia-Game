2ab6e876f6d27587d3514b1d2b00204a50abbe71
Ben Goligowski <51453763+btgoligowski24@users.noreply.github.com>
2019-07-27 08:23:06 -0500

    changes to styling and base questions figured out

---
+++ assets/javascript/app.js
@@ -0,0 +1,67 @@
+var triviaGame = {
+        questions: {
+            1: "What's the name of the episode that has the Trivia Game?",
+            2: "How many times has Ross been divorced?",
+            3: "What doesn't Joey share?",
+            4: "How many long-stemmed roses did Ross send to Emily?",
+            5: "How many lasagnas did Monica make for her aunt?",
+            6: "What is Monica's apartment number?",
+            7: "What is the name of Joey's stuffed penguin?",
+            8: "What is Chandler's middle name?",
+            9: "What did Joey name his barcalounger?",
+            10: "What was Chandler's job in the early seasons of the show?"
+            11: "Who pees on Monica's leg when she gets stung by a jellyfish?",
+            12: "What caused the fire in Phoebe and Rachel's apartment?",
+            13: "Who lives in the apartment below Monica's?"
+            14: "What color is the couch in Central Perk?"
+            15: "Who sang the Friends theme?",
+            16: "How many types of towels does Monica have?",
+            17: "What is Danny Devito's stripper name?",
+            18: "What does Paul (Bruce Willis) call himself to build his confidence?"
+            19: "Which famous person does Phoebe believe is her grandfather?",
+            20: "Where does David the scientist (Phoebe's boyfriend) move to?"
+        },
+        answers: {
+            1: "The One with the Embyros",
+            2: "3",
+            3: "Food",
+            4: "72",
+            5: "12",
+            6: "20",
+            7: "Hugsy",
+            8: "Muriel",
+            9: "Rosita",
+            10: "IT Procurement Manager"
+            11: "Chandler",
+            12: "Rachel's hair straightener",
+            13: "Mr. Heckles",
+            14: "Orange",
+            15: "The Rembrandts",
+            16: "11",
+            17: "Officer Goodbody",
+            18: "A neat guy",
+            19: "Albert Einstein",
+            20: "Minsk"
+        },
+        choices: {
+            1: ["The One with the Contest", "The One with Ross's Thing", "The One Where They Switch Apartments", "The One with the Embyros"],
+            2: ["1", "2", "3", "4"],
+            3: ["Women", "Food", "Money", "Advice"],
+            4: ["72", "100", "50", "24"],
+            5: ["6", "12", "2", "8"],
+            6: ["15", "18", "20", "12"],
+            7: ["Snuggles", "Hugsy", "Mr. Penguin", "None"],
+            8: ["Marcel", "Michelle", "Milicent", "Muriel"],
+            9: ["Rosita", "Rosa", "Rosie", "Rosalind"],
+            10: ["IT Procurement Manager", "Data Analyst", "Transpondster", "Financial Analyst"],
+            11: ["Monica", "Joey", "Chandler", "Richard"],
+            12: ["Phoebe's candles", "Phoebe's incense", "Rachel's hair dryer", "Rachel's hair straightener"],
+            13: ["Mr. Heckles", "Mr. Annoying", "Mr. Creepy", "Mr. Chill"],
+            14: ["Green", "Orange", "Yellow", "Red"],
+            15: ["Fool's Garden", "Smash Mouth", "Sunrise Avenue", "The Rembrandts"],
+            16: ["9", "10", "11", "12"],
+            17: ["Officer Goodbody", "Officer Sexy", "Officer Hunky", "Officer Lovely"]
+            18: ["A neat guy", "A cool guy", "A special guy", "A good guy"],
+            19: ["Thomas Edison", "Albert Einstein", "Nikola Tesla", "JP Morgan"],
+            20: ["Yemen", "Minsk", "Bulgaria", "Mongolia"]
+        }
