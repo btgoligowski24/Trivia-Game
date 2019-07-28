d0a5116f3cf80577a32154715235f775186f888a
Ben Goligowski <51453763+btgoligowski24@users.noreply.github.com>
2019-07-28 17:51:36 -0500

    added app.js to html and added functions to shuffle arrays and display questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,6 @@
-var triviaGame = {
+$(document).ready(function () {
+
+    var triviaGame = {
         questions: {
             1: "What's the name of the episode that has the Trivia Game?",
             2: "How many times has Ross been divorced?",
@@ -19,7 +21,7 @@ var triviaGame = {
             17: "What is Danny Devito's stripper name?",
             18: "What does Paul (Bruce Willis) call himself to build his confidence?",
             19: "Which famous person does Phoebe believe is her grandfather?",
-            20: "Where does David (Phoebe's boyfriend) move to?"
+            20: "Where does David, Phoebe's boyfriend, move to?"
         },
         answers: {
             1: "The One with the Embyros",
@@ -31,7 +33,7 @@ var triviaGame = {
             7: "Hugsy",
             8: "Muriel",
             9: "Rosita",
-            10: "IT Procurement Manager"
+            10: "IT Procurement Manager",
             11: "Chandler",
             12: "Rachel's hair straightener",
             13: "Mr. Heckles",
@@ -64,4 +66,78 @@ var triviaGame = {
             18: ["A neat guy", "A cool guy", "A special guy", "A good guy"],
             19: ["Thomas Edison", "Albert Einstein", "Nikola Tesla", "JP Morgan"],
             20: ["Yemen", "Minsk", "Bulgaria", "Mongolia"]
-        }
+        },
+        right: 0,
+        wrong: 0,
+        skipped: 0,
+        get questionCount() {
+            return Object.keys(this.questions).length;
+        },
+        counter: 1,
+        shuffle: function (obj1, obj2, obj3) {
+            var index = obj1.length;
+            var random, temp1, temp2, temp3;
+            if (arguments.length === 3) {
+                while (index) {
+                    random = Math.floor(Math.random() * index);
+                    index -= 1;
+                    temp1 = obj1[index];
+                    temp2 = obj2[index];
+                    temp3 = obj3[index];
+                    obj1[index] = obj1[random];
+                    obj2[index] = obj2[random];
+                    obj3[index] = obj3[random];
+                    obj1[random] = temp1;
+                    obj2[random] = temp2;
+                    obj3[random] = temp3;
+                }
+            } else if (arguments.length === 2) {
+                while (index) {
+                    random = Math.floor(Math.random() * index);
+                    index -= 1;
+                    temp1 = obj1[index];
+                    temp2 = obj2[index];
+                    obj1[index] = obj1[random];
+                    obj2[index] = obj2[random];
+                    obj1[random] = temp1;
+                    obj2[random] = temp2;
+                }
+            } else if (arguments.length === 1) {
+                while (index) {
+                    random = Math.floor(Math.random() * index);
+                    index -= 1;
+                    temp1 = obj1[index];
+                    obj1[index] = obj1[random];
+                    obj1[random] = temp1;
+                }
+            } else {
+                throw new Error("illegal argument count, accepts 1, 2, or 3");
+            }
+        },
+        displayQuestion: function () {
+            // run timer function
+            var newChoices = triviaGame.choices[triviaGame.counter];
+            triviaGame.shuffle(newChoices);
+            var index = triviaGame.counter;
+
+            if ($("#start").css("display") === "inline-block" && triviaGame.counter === 1) {
+                $("#start").css("display", "none");
+                $("#game").css("display", "block");
+            }
+            if (triviaGame.counter < triviaGame.questionCount) {
+                $("#answers").empty();
+                $("#question").text(triviaGame.questions[triviaGame.counter]);
+                triviaGame.counter++;
+                $(newChoices).each(function (i, val) {
+                    var newP = $("<p>");
+                    $(newP).text(val);
+                    $("#answers").append(newP);
+                })
+            }
+        },
+
+    }
+
+    $("#start").on("click", triviaGame.displayQuestion);
+
+})
